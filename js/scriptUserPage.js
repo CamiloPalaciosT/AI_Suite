d22b669ee7f3d4220cbe35a039fef74bee56b4c3
Camilo Palacios <124638638+CamiloPalaciosT@users.noreply.github.com>
2023-05-17 21:19:15 -0500

    se configuro un acordeón para la columnasChat

---
+++ js/scriptUserPage.js
@@ -0,0 +1,39 @@
+// chatGPT
+const btnAgregarCodigo = document.getElementById("btnAgregarCodigo");
+const contenedorCodigo = document.getElementById("contenedorCodigo");
+function agregarCodigoHTML() {
+const codigoHTML = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+contenedorCodigo.innerHTML += codigoHTML;
+}
+
+btnAgregarCodigo.addEventListener("click", agregarCodigoHTML);
+
+//BARD
+const btnAgregarCodigoDos = document.getElementById("btnAgregarCodigoDos");
+const contenedorCodigoDos = document.getElementById("contenedorCodigoDos");
+function agregarCodigoHTMLDos() {
+const codigoHTMLDos = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+contenedorCodigoDos.innerHTML += codigoHTMLDos;
+}
+
+btnAgregarCodigoDos.addEventListener("click", agregarCodigoHTMLDos);
+
+//AIChat
+const btnAgregarCodigoTres = document.getElementById("btnAgregarCodigoTres");
+const contenedorCodigoTres = document.getElementById("contenedorCodigoTres");
+function agregarCodigoHTMLTres() {
+const codigoHTMLTres = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+contenedorCodigoTres.innerHTML += codigoHTMLTres;
+}
+
+btnAgregarCodigoTres.addEventListener("click", agregarCodigoHTMLTres);
+
+//QuizCat
+const btnAgregarCodigoCuatro = document.getElementById("btnAgregarCodigoCuatro");
+const contenedorCodigoCuatro = document.getElementById("contenedorCodigoCuatro");
+function agregarCodigoHTMLCuatro() {
+const codigoHTMLCuatro = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+contenedorCodigoCuatro.innerHTML += codigoHTMLCuatro;
+}
+
+btnAgregarCodigoCuatro.addEventListener("click", agregarCodigoHTMLCuatro);
