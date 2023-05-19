cb6f0e57d6f6336e4a9dc6e36b840a7a6867210e
Camilo Palacios <124638638+CamiloPalaciosT@users.noreply.github.com>
2023-05-19 11:30:39 -0500

    mejor ui-style, y nuevo menu de ajustes

--- js/scriptUserPage.js
+++ js/scriptUserPage.js
@@ -1,17 +1,29 @@
-//---- clase Activa ---//
-var claseChat = document.getElementsByClassName("claseChat");
+//mensaje del chat
+const respuestaChat = `Lo siento aun no puedo entenderte, me encuentro en fase Beta. `
+/*----menu inicial----*/
+const menuToogle= document.querySelector('.menuToogle')
+const navegacion = document.querySelector('.navegacion')
 
-var claseChat = document.getElementsByClassName("claseChat");
-var boton = document.getElementById("miBoton");
-
-function ClaseActiva() {
-  Array.from(claseChat).forEach(function(element) {
-    element.classList.toggle("active");
-  });
+menuToogle.onclick =function(){
+    navegacion.classList.toggle('open')
 }
-
-boton.addEventListener("click", ClaseActiva);
-
+const list = document.querySelectorAll('.list')
+function activarLink(){
+    list.forEach((item)=>
+    item.classList.toggle('active'))
+}
+list.forEach((item)=>
+item.addEventListener('click', activarLink)
+)
+/*------clase activa-----*/
+var claseChat = document.querySelectorAll('.claseChat');
+function activarLink(event) {
+    var boton = event.currentTarget;
+    boton.classList.toggle('active');
+}
+claseChat.forEach(function(item) {
+    item.addEventListener('click', activarLink);
+});
 // chatGPT
 const btnAgregarCodigo = document.getElementById("btnAgregarCodigo");
 const contenedorCodigo = document.getElementById("contenedorCodigo");
@@ -19,13 +31,11 @@ const ContadorChatGPT = document.getElementById("contadorChatGPT");
 let contador = 0;
 function agregarCodigoHTML() {
 contador ++;
-const codigoHTML = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+const codigoHTML = '<div class="scene"><div class="cube"><span class="side top">' +'chat No.'+  contador  + '</span><span class="side front">' +'chat No.'+  contador  + '</span></div></div>';
 contenedorCodigo.innerHTML += codigoHTML;
 ContadorChatGPT.innerHTML = contador + " chats";
 }
-
 btnAgregarCodigo.addEventListener("click", agregarCodigoHTML);
-
 //BARD
 const btnAgregarCodigoDos = document.getElementById("btnAgregarCodigoDos");
 const contenedorCodigoDos = document.getElementById("contenedorCodigoDos");
@@ -33,13 +43,11 @@ const contadorBard = document.getElementById("contadorBard");
 let contadorDos = 0;
 function agregarCodigoHTMLDos() {
 contadorDos ++;
-const codigoHTMLDos = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+const codigoHTMLDos = '<div class="scene"><div class="cube"><span class="side top">' +'chat No.'+  contadorDos  + '</span><span class="side front">' +'chat No.'+  contadorDos  + '</span></div></div>';
 contenedorCodigoDos.innerHTML += codigoHTMLDos;
 contadorBard.innerHTML = contadorDos + " chats";
 }
-
 btnAgregarCodigoDos.addEventListener("click", agregarCodigoHTMLDos);
-
 //AIChat
 const btnAgregarCodigoTres = document.getElementById("btnAgregarCodigoTres");
 const contenedorCodigoTres = document.getElementById("contenedorCodigoTres");
@@ -47,13 +55,11 @@ const contadorAIChat = document.getElementById("contadorAIChat");
 let contadorTres = 0;
 function agregarCodigoHTMLTres() {
 contadorTres ++;
-const codigoHTMLTres = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+const codigoHTMLTres = '<div class="scene"><div class="cube"><span class="side top">' +'chat No.'+  contadorTres  + '</span><span class="side front">' +'chat No.'+  contadorTres  + '</span></div></div>';
 contenedorCodigoTres.innerHTML += codigoHTMLTres;
 contadorAIChat.innerHTML = contadorTres + " chats";
 }
-
 btnAgregarCodigoTres.addEventListener("click", agregarCodigoHTMLTres);
-
 //QuizCat
 const btnAgregarCodigoCuatro = document.getElementById("btnAgregarCodigoCuatro");
 const contenedorCodigoCuatro = document.getElementById("contenedorCodigoCuatro");
@@ -61,13 +67,11 @@ const contadorQuizCat = document.getElementById("contadorQuizCat");
 let contadorCuatro = 0;
 function agregarCodigoHTMLCuatro() {
 contadorCuatro ++;
-const codigoHTMLCuatro = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+const codigoHTMLCuatro = '<div class="scene"><div class="cube"><span class="side top">' +'chat No.'+  contadorCuatro  + '</span><span class="side front">' +'chat No.'+  contadorCuatro  + '</span></div></div>';
 contenedorCodigoCuatro.innerHTML += codigoHTMLCuatro;
 contadorQuizCat.innerHTML = contadorCuatro + " chats";
 }
-
 btnAgregarCodigoCuatro.addEventListener("click", agregarCodigoHTMLCuatro);
-
 //Silky
 const btnAgregarCodigoCinco = document.getElementById("btnAgregarCodigoCinco");
 const contenedorCodigoCinco = document.getElementById("contenedorCodigoCinco");
@@ -75,37 +79,34 @@ const contadorSilky = document.getElementById("contadorSilky");
 let contadorCinco = 0;
 function agregarCodigoHTMLCinco() {
 contadorCinco ++;
-const codigoHTMLCinco = '<button id="btnAgregarCodigo">Agregar Código HTML</button><div id="contenedorCodigo"></div>';
+const codigoHTMLCinco = '<div class="scene"><div class="cube"><span class="side top">' +'chat No.'+  contadorCinco  + '</span><span class="side front">' +'chat No.'+  contadorCinco  + '</span></div></div>';
 contenedorCodigoCinco.innerHTML += codigoHTMLCinco;
 contadorSilky.innerHTML = contadorCinco + " chats";
 }
-
 btnAgregarCodigoCinco.addEventListener("click", agregarCodigoHTMLCinco);
-
 //respuesta de chatGPT
 const zonaChat = document.getElementsByClassName("zonaChat")[0];
-
 function enviarMensaje() {
     var mensaje = document.getElementById("mensajeInput").value;
     procesarMensaje(mensaje);
-    document.getElementById("mensajeInput").value = ""; // Agregado: borrar el valor del campo de entrada
+    document.getElementById("mensajeInput").value = "";
 }
-
 function procesarMensaje(mensaje) {
-    zonaChat.innerHTML += `<div class="contenedorResultadoUsuario">
-    <div id="resultadoChat">
-        <div class="cuadroResultadoUsuario">` +
-        mensaje +
-        `</div>
+    zonaChat.innerHTML += 
+    `<div class="contenedorResultadoUsuario">
+        <div id="resultadoChat">
+            <div class="cuadroResultadoUsuario">` 
+                + mensaje +
+            `</div>
+        </div>
     </div>
-</div>
-<div class="contenedorResultadoChat">
-                        <div id="resultadoChat">
-                            <div class="cuadroResultadoChat">
-                                Lo siento aun no puedo entenderte, me encuentro en fase Beta. 
-                            </div>
-                        </div>
-                    </div>`;
+    <div class="contenedorResultadoChat">
+        <div id="resultadoChat">
+            <div class="cuadroResultadoChat">`
+                + respuestaChat +
+            `</div>
+        </div>
+    </div>`;
     console.log("Mensaje recibido: " + mensaje);
     zonaChat.scrollTop = zonaChat.scrollHeight;
 }
@@ -117,6 +118,3 @@ mensajeInput.addEventListener("keydown", function(event) {
     enviarMensaje();
     }
 });
-/*-------scroll zona chat -------*/
-
-window.addEventListener("DOMContentLoaded", scrollAlFinal);
